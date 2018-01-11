29a5188defe27edd62e6452f59f109559b73ecea
klanc <lewis@ledgedash.com>
2018-01-11 03:08:10 +0059

    El Recommit

---
+++ MIp1-mi.h
@@ -0,0 +1,25 @@
+/**************************************************************************/
+/*                                                                        */
+/* P1 - MI amb sockets TCP/IP - Part I                                    */
+/* Fitxer capçalera de mi.c                                               */
+/*                                                                        */
+/* Autors: X, Y                                                           */
+/*                                                                        */
+/**************************************************************************/
+
+
+/* Declaració de funcions externes de mi.c, és a dir, d'aquelles que es   */
+/* faran servir en un altre fitxer extern, p.e., MIp1-p2p.c; el fitxer    */
+/* extern farà un #include del fitxer .h a l'inici, i així les funcions   */
+/* seran conegudes en ell.                                                */
+/* En termes de capes de l'aplicació, aquest conjunt de funcions externes */
+/* formen la interfície de la capa MI.                                    */
+int MI_IniciaEscPetiRemConv(int portTCPloc, int* portFinal, char* ip);
+int MI_HaArribatPetiConv(int SckEscMI);
+int MI_DemanaConv(const char *IPrem, int portTCPrem, char *IPloc, int *portTCPloc, const char *NicLoc, char *NicRem);
+int MI_AcceptaConv(int SckEscMI, char *IPrem, int *portTCPrem, char *IPloc, int *portTCPloc, const char *NicLoc, char *NicRem);
+int MI_HaArribatLinia(int SckConvMI);
+int MI_EnviaLinia(int SckConvMI, const char *Linia);
+int MI_RepLinia(int SckConvMI, char *Linia);
+int MI_AcabaConv(int SckConvMI);
+int MI_AcabaEscPetiRemConv(int SckEscMI);
---
+++ MIp2-p2p.c
@@ -0,0 +1,104 @@
+/**************************************************************************/
+/*                                                                        */
+/* P1 - MI amb sockets TCP/IP - Part I                                    */
+/* Fitxer p2p.c que implementa la interfície aplicació-usuari de          */
+/* l'aplicació de MI, sobre la capa d'aplicació de MI (fent crides a la   */
+/* interfície de la capa MI -fitxers mi.c i mi.h-).                       */
+/* Autors: Lluís Trilla, Ismael El Habri                                  */
+/*                                                                        */
+/**************************************************************************/
+
+
+/* Inclusió de llibreries, p.e. #include <stdio.h> o #include "meu.h"     */
+/* Incloem "MIp1v4-mi.h" per poder fer crides a la interfície de MI       */
+#include "MIp1-mi.h"
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+
+
+/* Definició de constants, p.e., #define MAX_LINIA 150                    */
+
+
+int main(int argc,char *argv[])
+{
+	int portloc, sesc, sck_rep, portrem, scon, bytes_llegits, bytes_escrits,sckRep_Conv;
+	char iprem[16];
+	char iploc[16];
+	char nick[16];
+	char nickRem[16];
+	char linia[300];
+	char buffer[304];
+	int opcio=1;
+ /* Declaració de variables, p.e., int n;                                 */
+	strcpy(iploc, "0.0.0.0");
+	char ipMostrar[16];
+	int portMostrar;
+	if ((sesc = MI_IniciaEscPetiRemConv(0,&portMostrar, ipMostrar)) == -1) {
+		perror("socket\n");
+		exit(-1);
+	}
+    printf("IP@port: %s@%u\n", ipMostrar,portMostrar);
+	while(opcio!=0){
+		printf("entra 0 per sortir, o un qualsevol per iniciar conversació, o espera connexió:\n");
+		sck_rep = MI_HaArribatPetiConv(sesc);
+	 /* Expressions, estructures de control, crides a funcions, etc.          */
+		if (sck_rep == 0) { // TECLAT
+			scanf("%i",&opcio);
+			if (opcio==0) break;
+			printf("Entra IP a la que et vols connectar:\n");
+			bytes_llegits = TCP_Rep(0, iprem, sizeof(iprem));
+			printf("Entra el port al que et vols connectar:\n");
+			scanf("%i", &portrem);
+			printf("Entrar nick \n");
+			bytes_llegits = TCP_Rep(0,nick,sizeof(nick));
+			nick[bytes_llegits-1]='\0';
+			if((scon = MI_DemanaConv(iprem, portrem, iploc,0, nick, nickRem))==-1){
+				printf("error demanaConv\n");
+				exit(-1);
+			}
+		}
+		else { // SOCKET
+			printf("Entrar nick \n");
+			bytes_llegits = TCP_Rep(0,nick,sizeof(nick));
+			nick[bytes_llegits-1]='\0';
+			if ((scon=MI_AcceptaConv(sesc, iprem, &portrem, iploc, portloc, nick, nickRem))==-1){
+				printf("error acceptaConv\n");
+				exit(-1);
+			}
+		}
+		iprem[15]='\0';
+		ipMostrar[15]='\0';
+		//printf("Local IP@port: %s@%u\n", ipMostrar,portMostrar);
+		printf("Remot IP@port: %s@%u\n", iprem,portrem);
+		printf("conversi\n");
+		do{
+			sckRep_Conv = MI_HaArribatLinia(scon);
+			if (sckRep_Conv==0){ //teclat
+				bytes_llegits = read(0,linia,sizeof(linia));
+				if(linia[0] == ':') break;
+				linia[bytes_llegits-1]='\0'; //perpoder fer strlen, si passa salt de linia, fer -1
+				bytes_escrits = MI_EnviaLinia(scon, linia);
+			}else{ //socket
+				bytes_llegits = MI_RepLinia(sckRep_Conv, linia);
+				if(bytes_llegits!=-2) printf("%s: %s\n", nickRem, linia);
+			}
+
+		}while(bytes_llegits!=-2);
+		MI_AcabaConv(scon);
+	}
+
+	MI_AcabaEscPetiRemConv(sesc); //Tencar escolta al tencar bucle
+
+
+	return 0;
+ }
