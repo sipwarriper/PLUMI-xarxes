dc520b6a91b94bdf4a2b7bb0f4bf3f704eac6888
sipwarriper <sipwarriper@gmail.com>
2018-02-06 23:33:26 +0100

    dobles enviaments inicials solventats!

--- uploadCompile.sh
+++ uploadCompile.sh
@@ -6,4 +6,4 @@ ssh -i /mnt/ntfs/mx300/SSHKEYS/id_rsa.key root@ovh.ledgedash.com "cd /opt/xarxes
 echo "Copiant a Aruba"
 scp -i /mnt/ntfs/mx300/SSHKEYS/id_rsa.key ~/CLionProjects/PLUMI-xarxes/*.c ~/CLionProjects/PLUMI-xarxes/*.h root@aruba.ledgedash.com:/opt/xarxes/
 echo "Compilant a Aruba"
-ssh -i /mnt/ntfs/mx300/SSHKEYS/id_rsa.key root@Aruba.ledgedash.com "cd /opt/xarxes;gcc MIp2-lumi.c MIp2-mi.c MIp2-nodelumi.c -o servidor -w -std=c11;gcc MIp2-lumi.c MIp2-mi.c MIp2-p2p.c -o client -w -std=c11;" 
+ssh -i /mnt/ntfs/mx300/SSHKEYS/id_rsa.key root@Aruba.ledgedash.com "cd /opt/xarxes;gcc MIp2-lumi.c MIp2-mi.c MIp2-nodelumi.c -o servidor -w -std=c11;gcc MIp2-lumi.c MIp2-mi.c MIp2-p2p.c -o client -w -std=c11;"
